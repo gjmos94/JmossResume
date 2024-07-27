0767a033d94be2fe037c59955119494892fc8861
Gregg Moscosa <gregitis@outlook.com>
2024-07-27 01:48:09 -0500

    Initial commit for resume page

---
+++ server.js
@@ -0,0 +1,42 @@
+let express = require('express'); 
+let path = require('path'); 
+const sqlite3 = require('sqlite3'); 
+const { open } = require('sqlite'); 
+
+const PORT=8080; 
+
+const static_dir = path.join(__dirname, 'static');
+let db;
+
+(async () => { 
+    console.log("Making DB connection...  :o")
+    db = await open({ 
+        filename: 'resume.sqlite', 
+        driver: sqlite3.Database 
+    }); 
+    console.log("DB CONNECTION READY  #_#")
+})(); 
+
+const app = express();
+app.use(express.static(static_dir));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+/////////////////////////////////////////////
+///////////////////ROUTES////////////////////
+/////////////////////////////////////////////
+
+app.get('/test', async (req, res) => {
+    const result = await db.get("SELECT * FROM project WHERE id = 1");
+    console.log(result);
+    res.send("OKKK :D")
+    const projectResult = await db.all("SELECT * FROM project");
+    console.log(projectResult);
+});
+
+app.get('/resume', async (req, res) => {
+    const projectResult = await db.all("SELECT * FROM project");
+    res.render('resume', {projects: projectResult});
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));

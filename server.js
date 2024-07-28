f9a99c841dfd842e24d9149f3d058d8968ccbab6
Gregg Moscosa <gregitis@outlook.com>
2024-07-28 04:45:20 -0500

    database implementation for carousel and projects page

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-let express = require('express'); 
-let path = require('path'); 
+const express = require('express'); 
+const path = require('path'); 
 const sqlite3 = require('sqlite3'); 
 const { open } = require('sqlite'); 
 
@@ -30,42 +30,25 @@ app.get('/test', async (req, res) => {
     const result = await db.get("SELECT * FROM project WHERE id = 1");
     console.log(result);
     res.send("OKKK :D")
-    const projectResult = await db.all("SELECT * FROM project");
-    console.log(projectResult);
 });
 
-app.get('/resume', (req, res) => {
-    const projects = [
-        {
-            name: 'Project 1',
-            description: 'Brief description of Project 1.',
-            image: 'https://via.placeholder.com/800x400?text=Project+1'
-        },
-        {
-            name: 'Project 2',
-            description: 'Brief description of Project 2.',
-            image: 'https://via.placeholder.com/800x400?text=Project+2'
-        },
-        {
-            name: 'Project 3',
-            description: 'Brief description of Project 3.',
-            image: 'https://via.placeholder.com/800x400?text=Project+3'
-        },
-        {
-            name: 'Project 4',
-            description: 'Brief description of Project 4.',
-            image: 'https://via.placeholder.com/800x400?text=Project+4'
-        }
-    ];
-    res.render('resume', { projects });
+app.get('/resume', async (req, res) => {
+    try {
+        const projects = await db.all("SELECT * FROM project");
+        res.render('resume', { projects });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 app.get('/skills', (req, res) => {
     res.render('skills', {title: "Skills"});
 });
 
-app.get('/projects', (req, res) => {
-    res.render('projects', {title: "Projects"});
+app.get('/projects', async (req, res) => {
+    const projects = await db.all("SELECT * FROM project");
+    res.render('projects', {title: "Projects", projects });
 });
 
 app.get('/contact', (req, res) => {
